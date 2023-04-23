dd068eda15dc083bfcaba624f8a8afad8c2f0d70
Pranay <pranaydwivedi32@gmail.com>
2023-04-23 11:08:47 +0530

    Header file completed
    Header file completed

---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Header.module.css";
+
+function Header() {
+  return (
+    <div className={classes.header}>
+      <span> 🎥 Filmy Feast 🎬 </span>
+    </div>
+  );
+}
+
+export default Header;
